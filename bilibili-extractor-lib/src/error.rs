b5efe034cf6478cfde1221f13746e79cde682e1a
nanashi-1 <bagaresalken@gmail.com>
2023-08-27 10:29:33 +0800

    add new error type

---
+++ bilibili-extractor-lib/src/error.rs
@@ -0,0 +1,16 @@
+pub enum Error {
+    IOError(std::io::Error),
+    SerdeJSONError(serde_json::Error),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::IOError(value)
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(value: serde_json::Error) -> Self {
+        Self::SerdeJSONError(value)
+    }
+}
--- bilibili-extractor-lib/src/lib.rs
+++ bilibili-extractor-lib/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod error;
 pub mod ffmpeg_controller;
 pub mod metadata_reader;
 pub mod packager;
