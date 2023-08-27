23e3040a4164dd1080bc9f8b6a209e446276a1ac
nanashi-1 <bagaresalken@gmail.com>
2023-08-27 11:52:59 +0800

    add `ParseIntError` to Error

--- bilibili-extractor-lib/src/error.rs
+++ bilibili-extractor-lib/src/error.rs
@@ -2,6 +2,7 @@
 pub enum Error {
     IOError(std::io::Error),
     SerdeJSONError(serde_json::Error),
+    ParseIntError(std::num::ParseIntError),
 }
 
 impl From<std::io::Error> for Error {
@@ -13,5 +14,11 @@ impl From<std::io::Error> for Error {
 impl From<serde_json::Error> for Error {
     fn from(value: serde_json::Error) -> Self {
         Self::SerdeJSONError(value)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(value: std::num::ParseIntError) -> Self {
+        Self::ParseIntError(value)
     }
 }
