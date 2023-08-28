b83f8f98295a0e16a52ee63395787af615ae4e09
nanashi-1 <bagaresalken@gmail.com>
2023-08-28 08:10:40 +0800

    refactor packager

---
+++ bilibili-extractor-lib/src/packager.rs
@@ -0,0 +1,133 @@
+use crate::{
+    error::Error,
+    metadata::{NormalEpisodeMetadata, SeasonMetadata, SpecialEpisodeMetadata},
+};
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::{copy, rename},
+    path::Path,
+};
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Packager<P: AsRef<Path>> {
+    pub output_path: P,
+    pub config: PackagerConfig,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Default)]
+pub struct PackagerConfig {
+    pub copy: bool,
+}
+
+impl<P: AsRef<Path>> Packager<P> {
+    pub fn new(output_path: P) -> Self {
+        Self {
+            output_path,
+            config: Default::default(),
+        }
+    }
+
+    pub fn set_config(self, config: PackagerConfig) -> Self {
+        Self { config, ..self }
+    }
+
+    pub fn save_season(&self, season_metadata: SeasonMetadata<P>) -> Result<(), Error> {
+        season_metadata
+            .normal_episodes
+            .iter()
+            .map(|e| Ok(self.save_normal_episode(e)?))
+            .collect::<Result<_, Error>>()?;
+
+        season_metadata
+            .special_episodes
+            .iter()
+            .map(|e| Ok(self.save_special_episode(e)?))
+            .collect::<Result<_, Error>>()?;
+
+        Ok(())
+    }
+
+    pub fn save_normal_episode(
+        &self,
+        episode_metadata: &NormalEpisodeMetadata<P>,
+    ) -> Result<(), Error> {
+        let episode_video_path = episode_metadata
+            .path
+            .as_ref()
+            .ok_or(format!(
+                "Episode {} of {} doesn't contain a path",
+                episode_metadata.episode, episode_metadata.title
+            ))?
+            .as_ref()
+            .join(&episode_metadata.type_tag)
+            .join("episode.mkv");
+
+        if self.config.copy {
+            copy(
+                episode_video_path,
+                self.output_path
+                    .as_ref()
+                    .join(&episode_metadata.title)
+                    .join(format!(
+                        "{} EP{:0>2}.mkv",
+                        episode_metadata.title, episode_metadata.episode
+                    )),
+            )?;
+        } else {
+            rename(
+                episode_video_path,
+                self.output_path
+                    .as_ref()
+                    .join(&episode_metadata.title)
+                    .join(format!(
+                        "{} EP{:0>2}.mkv",
+                        episode_metadata.title, episode_metadata.episode
+                    )),
+            )?;
+        }
+
+        Ok(())
+    }
+
+    pub fn save_special_episode(
+        &self,
+        episode_metadata: &SpecialEpisodeMetadata<P>,
+    ) -> Result<(), Error> {
+        let episode_video_path = episode_metadata
+            .path
+            .as_ref()
+            .ok_or(format!(
+                "Episode {} of {} doesn't contain a path",
+                episode_metadata.episode_name, episode_metadata.title
+            ))?
+            .as_ref()
+            .join(&episode_metadata.type_tag)
+            .join("episode.mkv");
+
+        if self.config.copy {
+            copy(
+                episode_video_path,
+                self.output_path
+                    .as_ref()
+                    .join(&episode_metadata.title)
+                    .join(format!(
+                        "{} {}.mkv",
+                        episode_metadata.title, episode_metadata.episode_name
+                    )),
+            )?;
+        } else {
+            rename(
+                episode_video_path,
+                self.output_path
+                    .as_ref()
+                    .join(&episode_metadata.title)
+                    .join(format!(
+                        "{} {}.mkv",
+                        episode_metadata.title, episode_metadata.episode_name
+                    )),
+            )?;
+        }
+
+        Ok(())
+    }
+}
